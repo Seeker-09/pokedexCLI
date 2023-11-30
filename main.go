19eb0ebb13618c59f8a1666c9ce3b139a3189f40
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2023-11-30 18:20:26 -0500

    REPL

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		s.Scan()
+		input := s.Text()
+		fmt.Println(input)
+	}
 }
