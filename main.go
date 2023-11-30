f1ae4d0573f3e20ad5bdc751f70c253fefc2170b
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2023-11-30 18:53:59 -0500

    help command

--- main.go
+++ main.go
@@ -13,6 +13,32 @@ func main() {
 		fmt.Print("Pokedex > ")
 		s.Scan()
 		input := s.Text()
-		fmt.Println(input)
+		commands := getCommands()
+		command := commands[input]
+		command.action()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	action      func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help Message",
+			action:      helpAction,
+		},
+	}
+}
+
+func helpAction() {
+	for _, c := range getCommands() {
+		fmt.Println("Sure! Here is a list of commands: ")
+		fmt.Println("")
+		fmt.Printf("%s - %s\n", c.name, c.description)
 	}
 }
