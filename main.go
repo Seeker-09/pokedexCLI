7d1866d00f6193c3311e08ea0b23e08bed1fe98c
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2023-12-01 23:41:13 -0500

    Pokedex GET request

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Seeker-09/pokedexCLI/internal/pokeapi"
+	"log"
+)
+
 func main() {
-	startRepl()
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+	//startRepl()
 }
