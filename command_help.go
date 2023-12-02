6f122c1a78dc01a2b0719941e89ac41b7b9b31c8
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2023-12-01 23:12:00 -0500

    Building the REPL

---
+++ command_help.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func callBackHelp() error {
+	fmt.Println("Welcome to the Pokedex help menu")
+	fmt.Println("Here are your available comands:")
+
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
+	}
+
+	fmt.Println("")
+	return nil
+}
